2455dd8841837c172bd10cc265a81e36b02350ed
marcRDZ <marcos0rodriguez0diaz@gmail.com>
2014-03-30 21:18:30 +0200

    Latest settings

---
+++ Archdroid/src/main/java/com/theSoftwarersApprentice/archdroid/ArchMapFragment.java
@@ -0,0 +1,309 @@
+package com.theSoftwarersApprentice.archdroid;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SimpleAdapter;
+
+import com.actionbarsherlock.app.SherlockListFragment;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Ras-Mars on 14/02/14.
+ */
+public class ArchMapFragment extends SupportMapFragment implements GoogleMap.OnCameraChangeListener,
+        GoogleMap.OnMarkerClickListener, LocationListener {
+
+    private static final String LOG_TAG = "Archdroid";
+    private static final String PELAGIOS_BBOX_QUERY = "http://pelagios.dme.ait.ac.at/api/places.json?bbox=";
+    private static final int CREATE_MARKERS = 1415;
+    private static final int CREATE_PAGES = 9265;
+    private static final int CREATE_LISTS = 3589;
+    private String bounds, js, urlPlace, idMarker;
+    private Handler handler;
+    private DatasetsAdapter mPagerAdapter;
+    private ViewPager mPager;
+    private static List<HashMap<String,String>> annotations;
+
+    public ArchMapFragment() {
+
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+
+                Bundle b = msg.getData();
+                js = b.getString("data");
+
+                switch (b.getInt("switcher")){
+                    case CREATE_MARKERS:
+                        try {
+                            createMarkersFromJson(js);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }break;
+                    case CREATE_PAGES:
+                        try {
+                            createPagesFromJson(js);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }break;
+                    case CREATE_LISTS:
+                        try {
+                            createListFromJson(js);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }break;
+
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        LatLng mLatLng = new LatLng(location.getLatitude(),location.getLongitude());
+        getMap().animateCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(mLatLng, 15, 30, 0)));
+    }
+
+
+    @Override
+    public void onCameraChange(CameraPosition cameraPosition) {
+
+            LatLngBounds bBox = getMap().getProjection().getVisibleRegion().latLngBounds;
+            DecimalFormat df = new DecimalFormat("#.##");
+            bounds = df.format(bBox.southwest.longitude) +","+ df.format(bBox.southwest.latitude)
+                    +","+ df.format(bBox.northeast.longitude) +","+ df.format(bBox.northeast.latitude);
+
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+                       searchPelagiosData(PELAGIOS_BBOX_QUERY + bounds, CREATE_MARKERS);
+
+                    } catch (IOException e) {
+                        Log.e(LOG_TAG, "Cannot retrieve places", e);
+                    }
+                }
+            }).start();
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        //urlPlace = marker.getSnippet();
+
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    searchPelagiosData(urlPlace + "/datasets.json", CREATE_PAGES);
+
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, "Cannot retrieve datasets for this places", e);
+                }
+            }
+        }).start();
+
+        return false;
+    }
+
+    private void searchPelagiosData(String string, int switcher) throws IOException {
+        HttpURLConnection conn = null;
+        final StringBuilder json = new StringBuilder();
+        try {
+// Connect to the web service
+            URL url = new URL(string);
+            conn = (HttpURLConnection) url.openConnection();
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+// Read the JSON data into the StringBuilder
+            int read;
+            char[] buff = new char[1024];
+            while ((read = in.read(buff)) != -1) {
+                json.append(buff, 0, read);
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error connecting to service", e);
+            throw new IOException("Error connecting to service", e);
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+            sendToUIThread(json.toString(), switcher);
+        }
+    }
+
+    private void sendToUIThread(String string, int switcher) {
+        Message msg = handler.obtainMessage();
+        Bundle bundle = new Bundle();
+        bundle.putString("data", string);
+        bundle.putInt("switcher", switcher);
+        msg.setData(bundle);
+        handler.sendMessage(msg);
+    }
+
+    void createMarkersFromJson(String json) throws JSONException {
+
+        JSONArray jsonArray = new JSONArray(json);
+        for (int i = 0; i < jsonArray.length(); i++) {
+
+            JSONObject jsonObj = jsonArray.getJSONObject(i);
+            getMap().addMarker(new MarkerOptions().position(new LatLng(
+                    jsonObj.getJSONObject("geometry").getJSONArray("coordinates").optDouble(1),
+                    jsonObj.getJSONObject("geometry").getJSONArray("coordinates").optDouble(0)
+            )).snippet(jsonObj.getString("uri"))
+            //.icon(BitmapDescriptorFactory.fromResource(R.drawable.unknown))
+            );
+            urlPlace = jsonObj.getString("uri");
+            idMarker = jsonObj.getString("source");
+
+        }
+    }
+
+    void createPagesFromJson (String json) throws JSONException {
+
+        List<String[]> datasets = new ArrayList<String[]>();
+        String[] titleUri = new String[2];
+        JSONArray jsonArray = new JSONArray(json);
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObj = jsonArray.getJSONObject(i);
+            titleUri[0] = jsonObj.getString("uri");
+            titleUri[1] = jsonObj.getString("title");
+            datasets.add(titleUri);
+        }
+        mPagerAdapter = new DatasetsAdapter(getActivity().getSupportFragmentManager(), datasets);
+        mPager = (ViewPager)getActivity().findViewById(R.id.viewpager_layout);
+        mPager.setAdapter(mPagerAdapter);
+        mPager.setVisibility(View.VISIBLE);
+    }
+
+    private void createListFromJson(String json) throws JSONException {
+
+        JSONArray jsonArray = new JSONArray(json);
+        annotations = new ArrayList<HashMap<String,String>>();
+        HashMap<String, String> hm = new HashMap<String,String>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+
+            JSONObject jsonObj = jsonArray.getJSONObject(i);
+            jsonObj = jsonObj.getJSONObject("annotations");
+            hm.put("name", jsonObj.getString("target_title"));
+            hm.put("url", jsonObj.getString("hasTarget"));
+            annotations.add(hm);
+
+        }
+
+    }
+
+    public class AnnotationsFragment extends SherlockListFragment {
+
+        private String uri;
+
+        public AnnotationsFragment() {
+
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            uri = getArguments().getString("uri");
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        searchPelagiosData( uri + "/annotations.json?forPlace=" + idMarker, CREATE_LISTS);
+
+                    } catch (IOException e) {
+                        Log.e(LOG_TAG, "Cannot retrieve annotations for this place", e);
+                    }
+                }
+            }).start();
+
+        }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+
+
+
+            // Keys used in Hashmap
+            String[] from = {"name", "url"};
+            // Ids of views in listview_layout
+            int[] to = { R.id.name, R.id.url};
+            // Instantiating an adapter to store each items
+            // R.layout.list_layout defines the layout of each item
+            SimpleAdapter adapter = new SimpleAdapter(getActivity().getBaseContext(), annotations, R.layout.list_layout, from, to);
+            setListAdapter(adapter);
+
+            return super.onCreateView(inflater, container, savedInstanceState);
+        }
+
+        public AnnotationsFragment newInstance(int ord, int count, String title, String uri) {
+
+            AnnotationsFragment f = new AnnotationsFragment();
+            Bundle args = new Bundle();
+            args.putInt("ord", ord);
+            args.putInt("count", count);
+            args.putString("title", title);
+            args.putString("uri", uri);
+            f.setArguments(args);
+
+            return f;
+        }
+
+
+    }
+
+    public static class DatasetsAdapter extends FragmentPagerAdapter{
+
+        List<String[]> dsets;
+        String[] ttlUr;
+        AnnotationsFragment anFrag;
+
+        public DatasetsAdapter(FragmentManager fm, List<String[]> datasets) {
+            super(fm);
+            this.dsets = datasets;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            ttlUr = dsets.get(position);
+            return anFrag.newInstance(position, dsets.size(), ttlUr[0], ttlUr[1]);
+        }
+
+        @Override
+        public int getCount() {
+            return dsets.size();
+        }
+    }
+
+}
---
+++ Archdroid/src/main/java/com/theSoftwarersApprentice/archdroid/MainActivity.java
@@ -0,0 +1,195 @@
+package com.theSoftwarersApprentice.archdroid;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.actionbarsherlock.app.SherlockDialogFragment;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.GoogleMap;
+
+import static android.content.SharedPreferences.Editor;
+
+public class MainActivity extends SherlockFragmentActivity  implements
+        GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener{
+
+    private static final int REQUEST_CODE_RECOVER_PLAY_SERVICES = 1001;
+    private GoogleMap gMap;
+    private ArchMapFragment archMapFragment;
+    private ShareLocationDialogFragment mFragment;
+    private LocationClient mLocationClient;
+    private static final LocationRequest mLocationRequest = LocationRequest.create()
+            .setInterval(5000)
+            .setFastestInterval(16)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    protected boolean isUpdatesRequested;
+    protected SharedPreferences mPrefs;
+    protected Editor mEditor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mPrefs = getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE);
+        mEditor = mPrefs.edit();
+        getLocationUpdatesPreferences(mPrefs);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setUpMapIfNeeded();
+        setUpLocationClientIfNeeded();
+        if (isUpdatesRequested){
+            mLocationClient.connect();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        getLocationUpdatesPreferences(mPrefs);
+        setUpMapIfNeeded();
+        setUpLocationClientIfNeeded();
+        if (isUpdatesRequested) {
+            mLocationClient.connect();
+        }
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mLocationClient != null) {
+            mLocationClient.disconnect();
+        }
+        setLocationUpdatesPreferences(mEditor);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLocationClient.requestLocationUpdates(mLocationRequest, archMapFragment);
+    }
+
+    @Override
+    public void onDisconnected() {}
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(this, REQUEST_CODE_RECOVER_PLAY_SERVICES);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(), this,REQUEST_CODE_RECOVER_PLAY_SERVICES).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CODE_RECOVER_PLAY_SERVICES:
+                if (resultCode == RESULT_CANCELED) {
+                    Toast.makeText(this, "Google Play Services must be installed.", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void setUpMapIfNeeded() {
+        if (isPlayServicesInstalled()){
+
+            if (gMap == null) {
+                archMapFragment = (ArchMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_layout);
+                gMap = archMapFragment.getMap();
+            }
+            else {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        gMap.setOnCameraChangeListener(archMapFragment);
+        gMap.setOnMarkerClickListener(archMapFragment);
+    }
+
+    private void setUpLocationClientIfNeeded() {
+            if (mLocationClient == null) {
+                mLocationClient = new LocationClient(getApplicationContext(),this,this);
+            }
+    }
+
+    private boolean isPlayServicesInstalled() {
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (status != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(status)) {
+
+                GooglePlayServicesUtil.getErrorDialog(status, this,REQUEST_CODE_RECOVER_PLAY_SERVICES).show();
+            } else {
+                Toast.makeText(this, "This device is not supported.", Toast.LENGTH_LONG).show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private void getLocationUpdatesPreferences(SharedPreferences sharedPreferences) {
+        if (!sharedPreferences.contains("KEY_UPDATES_ON")) {
+            mFragment = new ShareLocationDialogFragment();
+            mFragment.show(getSupportFragmentManager(),"Location Updates");
+        } else {
+            isUpdatesRequested = sharedPreferences.getBoolean("KEY_UPDATES_ON", false);
+        }
+
+    }
+
+    private void setLocationUpdatesPreferences(Editor editor) {
+        editor.putBoolean("KEY_UPDATES_ON", isUpdatesRequested);
+        editor.commit();
+    }
+
+
+    public class ShareLocationDialogFragment extends SherlockDialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.share_location)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            isUpdatesRequested = true;
+                            //mFragment.dismiss();
+                        }
+                    })
+                    .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            isUpdatesRequested = false;
+                            //mFragment.dismiss();
+                        }
+                    });
+            return builder.create();
+        }
+    }
+
+}
